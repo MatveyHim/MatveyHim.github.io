00dd8d6fbdb0ae95ef670ff954e2d1a1317d0840
MatveyHim <98096943+MatveyHim@users.noreply.github.com>
2023-06-24 19:21:11 +0700

    index

--- action.php
+++ action.php
@@ -1,8 +1,22 @@
 <?php
+$time = $_POST['time'];
 $place = $_POST['place'];
 $spec = $_POST['spec'];
+$azot = $_POST['azot'];
+$azdo = $_POST['azdo'];
+$alt = $_POST['alt'];
+$filename = __DIR__ . '/file.txt';
 
-echo $fio;
+echo $time;
 echo "<br>";
-echo $email;
+echo $place;
+echo "<br>";
+echo $spec;
+echo $azot;
+echo "<br>";
+echo $azdo;
+echo "<br>";
+echo $alt;
+
+file_put_contents($filename, PHP_EOL .$time.' '.$place.' '.$azot.' '.$azdo.' '.$alt.' '.$spec , FILE_APPEND);
 ?>
